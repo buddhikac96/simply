970b013cf2d1661e47074128311df03bcba1ede4
Geethya <geethya1995@gmail.com>
2021-06-10 01:59:51 +0530

    simply float type mapped with java double

--- src/main/java/ast/util/enums/DataType.java
+++ src/main/java/ast/util/enums/DataType.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 
 public enum  DataType {
     @JsonAlias({"int"}) IntegerType("integer"),
-    @JsonAlias({"float"}) FloatType("float"),
+    @JsonAlias({"double"}) FloatType("float"),
     @JsonAlias({"boolean"}) BooleanType("boolean"),
     @JsonAlias({"char"}) CharType("char"),
     @JsonAlias({"String", "string"}) StringType("string"),
