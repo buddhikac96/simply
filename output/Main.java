a644786d54bfa011b25265c26dabe9e57f7a2fee
Buddhika Chathuranga <vmbckscc456@gmail.com>
2021-06-15 20:14:22 +0530

    Minor updates

---
+++ output/Main.java
@@ -0,0 +1,19 @@
+
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class Main {
+    static double PI = 3.14;
+
+    public static void main(String[] args) {
+        int a = 5; 
+        int b = 10; 
+        int total = a + b; 
+        System.out.println(total); 
+
+    }
+}
+
+
